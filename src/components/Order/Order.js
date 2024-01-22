99da515c51ca1bc93064a44105f19629320c54aa
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-01-22 17:31:23 +0200

    update Order.js

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,14 +1,15 @@
-// import { useDispatch } from 'react-redux';
 import css from './Order.module.css';
 
 export const Order = ({ name, number, dealer, plannedDeadline }) => {
+
+  const date = new Date(plannedDeadline);
 
   return (
     <div className={css.wrapper}>
       <p className={css.number}>{number}</p>
       <div className={css.discription}>
         <p>{name}</p>
-        <p>{Date.parse(plannedDeadline)}</p>
+        <p>{`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`}</p>
         <p>{dealer}</p>
       </div>
     </div>
