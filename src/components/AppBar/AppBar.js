f46fd56376cdbcff6ab445808c1e469458da6428
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-01-26 19:10:56 +0200

    Summary

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,16 +1,22 @@
 import { Navigation } from '../Navigation/Navigation';
+import { MobileMenuBtn } from '../MobileMenu/MobileMenuBtn'
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
+import { useMediaQuery } from 'react-responsive';
 import css from './AppBar.module.css';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
+  // const isDesktop = useMediaQuery({ query: '(min-width: 834px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 833px)' });
+
   return (
     <header className={css.header}>
       <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {isMobile ? <MobileMenuBtn /> : isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
     </header>
   );
 };
--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,13 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
+
+  const isMobile = useMediaQuery({ query: '(max-width: 833px)' });
+
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <NavLink className={isMobile ? css.mobileLink : css.link} to="/register">
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink className={isMobile ? css.mobileLink : css.link} to="/login">
         Log In
       </NavLink>
     </div>
