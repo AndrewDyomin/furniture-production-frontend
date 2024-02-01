7a92bbd929e5633a27169f8b03050f399da33c9c
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-02-01 10:18:11 +0200

    Update CollectionEditor.js

--- src/components/CollectionEditor/CollectionEditor.js
+++ src/components/CollectionEditor/CollectionEditor.js
@@ -41,20 +41,24 @@ export const CollectionEditor = () => {
                 <Field className={css.field} id="name" name="name" placeholder="Faynee mini" />
             </div>
             <div className={css.formItem}>
-            <label htmlFor="dimensions.width">Width</label>
-            <Field className={css.field} id="dimensions.width" name="dimensions.width" placeholder="Width"/>
-            </div>
-            <div className={css.formItem}>
-                <label htmlFor="dimensions.height">Height</label>
-                <Field className={css.field} id="dimensions.height" name="dimensions.height" placeholder="Height"/>
+                <label htmlFor="dimensions.width">Width</label>
+                <Field className={css.field} id="dimensions.width" name="dimensions.width" placeholder="Width"/>
             </div>
             <div className={css.formItem}>
                 <label htmlFor="dimensions.depth">Depth</label>
                 <Field className={css.field} id="dimensions.depth" name="dimensions.depth" placeholder="Depth"/>
             </div>
             <div className={css.formItem}>
+                <label htmlFor="dimensions.height">Height</label>
+                <Field className={css.field} id="dimensions.height" name="dimensions.height" placeholder="Height"/>
+            </div>
+            <div className={css.formItem}>
                 <label htmlFor="subscription">Subscription</label>
                 <Field className={css.field} id="subscription" name="subscription" placeholder="Subscription" />
+            </div>
+            <div className={css.formItem}>
+                <label htmlFor="price">Base price</label>
+                <Field className={css.field} id="price" name="price" placeholder="12500" />
             </div>
             <div className={css.formItem}>
                 <FieldArray
@@ -64,13 +68,13 @@ export const CollectionEditor = () => {
                         {arrayHelpers.form.values.images.map((image, index) => (
                             <div key={index} className={css.inputArray}>
                             <Field className={css.field} name={`images.${index}`} placeholder="images"/>
-                            <button
+                            {arrayHelpers.form.values.images.length > 1 ? <button
                                 className={css.minBtn}
                                 type="button"
                                 onClick={() => arrayHelpers.remove(index)}
                             >
                                 -
-                            </button>
+                            </button> : <></>}
                             {index === arrayHelpers.form.values.images.length - 1 && (
                                 <button
                                     className={css.minBtn}
@@ -94,13 +98,13 @@ export const CollectionEditor = () => {
                         {arrayHelpers.form.values.components.map((component, index) => (
                             <div key={index} className={css.inputArray}>
                             <Field className={css.field} name={`components.${index}`} placeholder="components"/>
-                            <button
+                            {arrayHelpers.form.values.components.length > 1 ? <button
                                 className={css.minBtn}
                                 type="button"
                                 onClick={() => arrayHelpers.remove(index)}
                             >
                                 -
-                            </button>
+                            </button> : <></>}
                             {index === arrayHelpers.form.values.components.length - 1 && (
                                 <button
                                     className={css.minBtn}
