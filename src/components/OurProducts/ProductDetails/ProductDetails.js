00ac21798a6dfb9900dd1e5cf344a376bcb56957
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-02-02 12:24:46 +0200

    update ProductDetails.js

--- src/components/OurProducts/ProductDetails/ProductDetails.js
+++ src/components/OurProducts/ProductDetails/ProductDetails.js
@@ -1,33 +1,53 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import { selectAllProducts } from '../../../redux/products/selectors';
-import css from "./ProductDetails.module.css"
+import css from "./ProductDetails.module.css";
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+const bedSizes = [
+  { value: '140', label: '140 x 200(190)' },
+  { value: '160', label: '160 x 200(190)' },
+  { value: '180', label: '180 x 200(190)' },
+  { value: '200', label: '200 x 200(190)' },
+];
 
 export const ProductDetails = () => {
+
+    const [selectedBedSize, setSelectedBedSize] = useState('160');
 
     const { id } = useParams();
     const products = useSelector(selectAllProducts);
     const product = products.array.find((el) => (el._id === id));
+    const price = product.basePrice;
 
     return(
         <div className={css.section}>
             <h1 className={css.title}>{product.name}</h1>
-            <Swiper
-                spaceBetween={0}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                >
-                {product.images.map((image) => (<SwiperSlide><img src={image} key={image} alt={product.group} className={css.image}/></SwiperSlide>))}
-                {/* <SwiperSlide key='1'>Slide 1</SwiperSlide>
-                <SwiperSlide key='2'>Slide 2</SwiperSlide>
-                <SwiperSlide key='3'>Slide 3</SwiperSlide>
-                <SwiperSlide key='4'>Slide 4</SwiperSlide> */}
-            </Swiper>
+            <Carousel>
+                {product.images.map((link) => (
+                    <div key={link}>
+                        <img src={link} alt={product.name}/>
+                    </div>
+                ))}
+            </Carousel>
+            {product.group === 'bed' ? 
+                <div>
+                    <p>Sleeping area:</p>
+                    <Select
+                        defaultValue={selectedBedSize}
+                        onChange={setSelectedBedSize}
+                        options={bedSizes}
+                    />
+                    <p>Overall size:</p>
+                    <p>HZ</p>
+                    <p>Price:</p>
+                    <p>{price}</p>
+                </div> : <></>}
+            <p>Description:</p>
+            <p>{product.subscription}</p>
         </div>
     )
 }
