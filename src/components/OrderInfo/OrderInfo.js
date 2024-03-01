8cffecff2210eb5303ba7fedec7ae3aa1f63cf05
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-03-01 16:34:20 +0200

    Summary

--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -1,9 +1,11 @@
 import css from './OrderInfo.module.css';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { selectActiveOrder } from '../../redux/orders/selectors';
 
 export const OrderInfo = ({ id }) => {
 
+  const { t } = useTranslation();
   const order = useSelector(selectActiveOrder);
   const date = new Date(order.plannedDeadline);
 
@@ -12,13 +14,13 @@ export const OrderInfo = ({ id }) => {
       <p className={css.orderNumber}>{order.number}</p>
       <div className={css.description}>
         <p className={css.orderName}>{order.group} {order.name}</p>
-        <p className={css.orderSize}>Size: {order.size}</p>
-        <p className={css.orderFabric}>Fabric: {order.fabric}</p>
-        <p className={css.orderDescription}>Description: {order.description}</p>
-        <p className={css.orderDeadline}>Deadline: {`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`}</p>
+        <p className={css.orderSize}>{t('size')}: {order.size}</p>
+        <p className={css.orderFabric}>{t('fabric')}: {order.fabric}</p>
+        <p className={css.orderDescription}>{t('description')}: {order.description}</p>
+        <p className={css.orderDeadline}>{t('deadline')}: {`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`}</p>
         <p>{order.dealer}</p>
-        <p className={css.orderAdress}>Adress: {order.adress}</p>
-        <p className={css.orderRest}>Rest: {order.rest}</p>
+        <p className={css.orderAdress}>{t('adress')}: {order.adress}</p>
+        <p className={css.orderRest}>{t('rest')}: {order.rest}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -50,7 +50,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/furniture-production-frontend">
+        {/* <BrowserRouter basename="/furniture-production-frontend"> */}
+        <BrowserRouter >
           <I18nextProvider i18n={i18n}>
             <App />
           </I18nextProvider>
