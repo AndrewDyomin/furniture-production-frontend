715adddbca4a4fc773cdd96995f8b4209e479736
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-03-19 11:12:19 +0200

    Summary

--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -6,10 +6,9 @@ import axios from 'axios';
 import { Formik, Field, Form, FieldArray } from 'formik';
 import { selectActiveOrder } from '../../redux/orders/selectors';
 import { selectUser } from '../../redux/auth/selectors';
-import { PopUp } from "components/PopUp/PopUp";
+import { PopUp } from 'components/PopUp/PopUp';
 
 export const OrderInfo = ({ id }) => {
-
   const { t } = useTranslation();
   const order = useSelector(selectActiveOrder);
   const user = useSelector(selectUser);
@@ -17,12 +16,14 @@ export const OrderInfo = ({ id }) => {
   function dateToString(date) {
     const d = new Date(date);
     const month = d.getMonth() + 1;
-    const dateString = `${d.getDate().toString().padStart(2, '0')}.${month.toString().padStart(2, '0')}.${d.getFullYear()}`;
+    const dateString = `${d.getDate().toString().padStart(2, '0')}.${month
+      .toString()
+      .padStart(2, '0')}.${d.getFullYear()}`;
     return dateString;
-  };
+  }
 
   // const date = new Date(order.plannedDeadline);
-  const [isModalEditOpen, setIsModalEditOpen] = useState(false)
+  const [isModalEditOpen, setIsModalEditOpen] = useState(false);
 
   const openEditModal = () => {
     setIsModalEditOpen(true);
@@ -37,35 +38,63 @@ export const OrderInfo = ({ id }) => {
     <div className={css.wrapper}>
       <p className={css.orderNumber}>{order.number}</p>
       <div className={css.description}>
-        <p className={css.orderName}>{order.group} {order.name}</p>
-        <p className={css.orderSize}>{t('size')}: {order.size}</p>
-        <p className={css.orderFabric}>{t('fabric')}: {order.fabric}</p>
-        <p className={css.orderDescription}>{t('description')}: {order.description}</p>
-        <p className={css.orderDeadline}>{t('deadline')}: {dateToString(order.plannedDeadline)}</p>
+        <p className={css.orderName}>
+          {order.group} {order.name}
+        </p>
+        <p className={css.orderSize}>
+          {t('size')}: {order.size}
+        </p>
+        <p className={css.orderFabric}>
+          {t('fabric')}: {order.fabric}
+        </p>
+        <p className={css.orderDescription}>
+          {t('description')}: {order.description}
+        </p>
+        <p className={css.orderDeadline}>
+          {t('deadline')}: {dateToString(order.plannedDeadline)}
+        </p>
         <p>{order.dealer}</p>
-        {user.description === 'seamstress' || user.description === 'carpenter' || user.description === 'upholsterer' ? <></> : 
-        <div>
-          <p className={css.orderAdress}>{t('adress')}: {order.adress}</p>
-          <p className={css.orderRest}>{t('rest')}: {order.rest}</p> 
-        </div>
-        }
+        {user.description === 'seamstress' ||
+        user.description === 'carpenter' ||
+        user.description === 'upholsterer' ? (
+          <></>
+        ) : (
+          <div>
+            <p className={css.orderAdress}>
+              {t('adress')}: {order.adress}
+            </p>
+            <p className={css.orderRest}>
+              {t('rest')}: {order.rest}
+            </p>
+          </div>
+        )}
       </div>
-      {order.images && order.images.length !== 0 ? 
-      <ul className={css.imagesList}>
-        {order.images.map((imageId) => (
-          <li key={`${imageId}`}>
-            <img className={css.orderImage} src={`https://lh3.googleusercontent.com/d/${imageId}=w800?authuser=0`} alt={imageId}/>
-            {/* <img src={`https://drive.google.com/thumbnail?id=${imageId}&sz=w800`} alt={imageId}/> */}
-          </li>
-        ))}
-      </ul> 
-      : <></>}
-      {user.description === 'administrator' ? 
-      <div>
-        <button className={css.btn} onClick={openEditModal}>{t('edit')}</button>
-      </div> 
-      : <></>}
-      <PopUp 
+      {order.images && order.images.length !== 0 ? (
+        <ul className={css.imagesList}>
+          {order.images.map(imageId => (
+            <li key={`${imageId}`}>
+              <img
+                className={css.orderImage}
+                src={`https://lh3.googleusercontent.com/d/${imageId}=w800?authuser=0`}
+                alt={imageId}
+              />
+              {/* <img src={`https://drive.google.com/thumbnail?id=${imageId}&sz=w800`} alt={imageId}/> */}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <></>
+      )}
+      {user.description === 'administrator' ? (
+        <div>
+          <button className={css.btn} onClick={openEditModal}>
+            {t('edit')}
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+      <PopUp
         isOpen={isModalEditOpen}
         close={closeEditModal}
         className={css.modalContent}
@@ -97,90 +126,142 @@ export const OrderInfo = ({ id }) => {
                 coverStatus: order.coverStatus,
                 frameStatus: order.frameStatus,
               }}
-              onSubmit={async (values) => {
-                  try {
-                    const formData = new FormData();
-                    formData.append('group', values.group);
-                    formData.append('size', values.size);
-                    formData.append('name', values.name);
-                    formData.append('fabric', values.fabric);
-                    formData.append('description', values.description);
-                    formData.append('base', values.base);
-                    formData.append('deliveryDate', values.deliveryDate);
-                    formData.append('innerPrice', values.innerPrice);
-                    formData.append('number', values.number);
-                    formData.append('dealer', values.dealer);
-                    formData.append('deadline', values.deadline);
-                    formData.append('dateOfOrder', values.dateOfOrder);
-                    formData.append('adress', values.adress);
-                    formData.append('additional', values.additional);
-                    formData.append('rest', values.rest);
-                    formData.append('plannedDeadline', values.plannedDeadline);
-                    formData.append('orderStatus', values.orderStatus);
-                    formData.append('_id', values._id);
-                    formData.append('images', values.images);
-                    formData.append('fabricStatus', values.fabricStatus);
-                    formData.append('coverStatus', values.coverStatus);
-                    formData.append('frameStatus', values.frameStatus);
-                  
-                    await axios.post('/orders/update', formData, {
-                        headers: {
-                            'Content-Type': 'multipart/form-data'
-                        }
-                    });
-                    console.log(formData)
-                    closeEditModal();
-                  } catch(error) {
-                    console.log(error);
-                  }
+              onSubmit={async values => {
+                try {
+                  const formData = new FormData();
+                  formData.append('group', values.group);
+                  formData.append('size', values.size);
+                  formData.append('name', values.name);
+                  formData.append('fabric', values.fabric);
+                  formData.append('description', values.description);
+                  formData.append('base', values.base);
+                  formData.append('deliveryDate', values.deliveryDate);
+                  formData.append('innerPrice', values.innerPrice);
+                  formData.append('number', values.number);
+                  formData.append('dealer', values.dealer);
+                  formData.append('deadline', values.deadline);
+                  formData.append('dateOfOrder', values.dateOfOrder);
+                  formData.append('adress', values.adress);
+                  formData.append('additional', values.additional);
+                  formData.append('rest', values.rest);
+                  formData.append('plannedDeadline', values.plannedDeadline);
+                  formData.append('orderStatus', values.orderStatus);
+                  formData.append('_id', values._id);
+                  formData.append('images', values.images);
+                  formData.append('fabricStatus', values.fabricStatus);
+                  formData.append('coverStatus', values.coverStatus);
+                  formData.append('frameStatus', values.frameStatus);
+
+                  await axios.post('/orders/update', formData, {
+                    headers: {
+                      'Content-Type': 'multipart/form-data',
+                    },
+                  });
+                  console.log(formData);
+                  closeEditModal();
+                } catch (error) {
+                  console.log(error);
+                }
               }}
-              >
+            >
               <Form className={css.formWrapper}>
                 <div className={css.formItem}>
-                    <label htmlFor="group">{t('group')}</label>
-                    <Field className={css.field} id="group" name="group" />
+                  <label htmlFor="group">{t('group')}</label>
+                  <Field className={css.field} id="group" name="group" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="size">{t('size')}</label>
-                    <Field className={css.field} id="size" name="size" />
+                  <label htmlFor="size">{t('size')}</label>
+                  <Field className={css.field} id="size" name="size" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="name">{t('order name')}</label>
-                    <Field className={css.field} id="name" name="name" />
+                  <label htmlFor="name">{t('order name')}</label>
+                  <Field className={css.field} id="name" name="name" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="fabric">{t('fabric')}</label>
-                    <Field className={css.field} id="fabric" name="fabric" />
+                  <label htmlFor="fabric">{t('fabric')}</label>
+                  <Field className={css.field} id="fabric" name="fabric" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="description">{t('description')}</label>
-                    <Field className={css.field} id="description" name="description" placeholder="Description" />
+                  <label htmlFor="description">{t('description')}</label>
+                  <Field
+                    className={css.field}
+                    id="description"
+                    name="description"
+                    placeholder="Description"
+                  />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="base">{t('base')}</label>
-                    <Field className={css.field} id="base" name="base" />
+                  <label htmlFor="base">{t('base')}</label>
+                  <Field className={css.field} id="base" name="base" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="innerPrice">{t('inner price')}</label>
-                    <Field className={css.field} id="innerPrice" name="innerPrice" />
+                  <label htmlFor="innerPrice">{t('inner price')}</label>
+                  <Field
+                    className={css.field}
+                    id="innerPrice"
+                    name="innerPrice"
+                  />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="number">{t('number')}</label>
-                    <Field className={css.field} id="number" name="number" />
+                  <label htmlFor="number">{t('number')}</label>
+                  <Field className={css.field} id="number" name="number" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="adress">{t('adress')}</label>
-                    <Field className={css.field} id="adress" name="adress" />
+                  <label htmlFor="adress">{t('adress')}</label>
+                  <Field className={css.field} id="adress" name="adress" />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="additional">{t('additional')}</label>
-                    <Field className={css.field} id="additional" name="additional" />
+                  <label htmlFor="additional">{t('additional')}</label>
+                  <Field
+                    className={css.field}
+                    id="additional"
+                    name="additional"
+                  />
                 </div>
                 <div className={css.formItem}>
-                    <label htmlFor="plannedDeadline">{t('planned deadline')}</label>
-                    <Field className={css.field} id="plannedDeadline" name="plannedDeadline" />
+                  <label htmlFor="plannedDeadline">
+                    {t('planned deadline')}
+                  </label>
+                  <Field
+                    className={css.field}
+                    id="plannedDeadline"
+                    name="plannedDeadline"
+                  />
                 </div>
-                <button type="submit" className={`${css.btn} ${css.modalSubmitButton}`}>{t('submit')}</button>
+                <div className={css.formItem}>
+                  <FieldArray
+                    name="images"
+                    render={arrayHelpers => (
+                      <div>
+                        {arrayHelpers.form.values.images.map(
+                          (component, index) => (
+                            <div key={index} className={css.inputArray}>
+                              <Field
+                                component={Select}
+                                className={css.selectComponent}
+                                name={`components.${index}`}
+                                onChange={e =>
+                                  setSelectedComponents(prevState => {
+                                    const updatedComponents = [...prevState];
+                                    updatedComponents[index] = e.value;
+                                    return updatedComponents;
+                                  })
+                                }
+                                options={componentList}
+                              ></Field>
+                            </div>
+                          )
+                        )}
+                      </div>
+                    )}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className={`${css.btn} ${css.modalSubmitButton}`}
+                >
+                  {t('submit')}
+                </button>
               </Form>
             </Formik>
           </>
