2444a02bd2536fbd9f0cbdddd04fbce48ae5dbb6
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-04-24 14:35:33 +0300

    Update MyRoom.js

--- src/pages/MyRoom.js
+++ src/pages/MyRoom.js
@@ -2,8 +2,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { CollectionEditor } from '../components/CollectionEditor/CollectionEditor';
 import { ComponentList } from '../components/ComponentList/ComponentList';
 import { OrderEditor } from '../components/OrderEditor/OrderEditor';
+import { fetchAllComponents } from '../redux/components/operations';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 export default function MyRoom() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllComponents());
+  }, [dispatch]);
 
   return (
     <>
