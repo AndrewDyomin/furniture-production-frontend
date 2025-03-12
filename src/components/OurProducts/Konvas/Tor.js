d8656d9de05db452327bd47abb7a9ce8a8d9c637
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2025-03-12 18:17:22 +0200

    Create Tor.js

---
+++ src/components/OurProducts/Konvas/Tor.js
@@ -0,0 +1,158 @@
+import { useState, useEffect, forwardRef, useImperativeHandle, useRef } from 'react';
+import { Stage, Layer, Rect, Line, Text } from 'react-konva';
+
+ const Tor = forwardRef(({
+  dimensions,
+  productWidth,
+  productDepth,
+}, ref) => {
+  const [scaleFactor, setScaleFactor] = useState(1);
+  const stageRef = useRef(null);
+
+  const sofaTotalDepth = productDepth * scaleFactor;
+  const sofaTotalWidth = productWidth * scaleFactor;
+
+  const armrestsWidth = 20 * scaleFactor
+  const armrestsDepth = sofaTotalDepth
+
+  const offsetX = dimensions.width / 2;
+  const offsetY = dimensions.height / 2;
+
+  useEffect(() => {
+    const value = Math.min(
+      (0.7 * dimensions.width) / productWidth,
+      (0.7 * dimensions.height) / productDepth
+    );
+    if (value > 0) {
+      setScaleFactor(value);
+    }
+  }, [dimensions, productWidth, productDepth]);
+
+  useImperativeHandle(ref, () => ({
+    getImage() {
+      return stageRef.current.toDataURL({ mimeType: 'image/jpeg', quality: 1 });
+    }
+  }));
+
+  return (
+    <div>
+      <Stage ref={stageRef} width={dimensions.width} height={dimensions.height}>
+        <Layer>
+        <Rect
+            x={0}
+            y={0}
+            width={dimensions.width}
+            height={dimensions.height}
+            fill={'#FFF'}
+          />
+        </Layer>
+        <Layer>
+            {/* back */}
+          <Rect
+            x={offsetX - (sofaTotalWidth / 2) + armrestsWidth}
+            y={offsetY - (sofaTotalDepth / 2) + 3 * scaleFactor}
+            width={sofaTotalWidth >= 100 ? sofaTotalWidth - (armrestsWidth * 2) : 10}
+            height={20 * scaleFactor}
+            stroke="black"
+            strokeWidth={1}
+            cornerRadius={3}
+          />
+          {/* seat */}
+          <Rect
+            x={offsetX - (sofaTotalWidth / 2) + armrestsWidth}
+            y={offsetY - (sofaTotalDepth / 2) + (23 * scaleFactor)}
+            width={sofaTotalWidth >= 100 ? sofaTotalWidth - (armrestsWidth * 2) : 10}
+            height={sofaTotalDepth - (23 * scaleFactor)}
+            stroke="black"
+            strokeWidth={1}
+            cornerRadius={3}
+          />
+          {/* armrests */}
+          <Rect
+            x={offsetX - (sofaTotalWidth / 2)}
+            y={offsetY - (sofaTotalDepth / 2)}
+            width={sofaTotalWidth >= 100 ? armrestsWidth : 10}
+            height={armrestsDepth}
+            stroke="black"
+            strokeWidth={1}
+            cornerRadius={3}
+          />
+          <Rect
+            x={offsetX + (sofaTotalWidth / 2) - armrestsWidth}
+            y={offsetY - (sofaTotalDepth / 2)}
+            width={sofaTotalWidth >= 100 ? armrestsWidth : 10}
+            height={armrestsDepth}
+            stroke="black"
+            strokeWidth={1}
+            cornerRadius={3}
+          />
+        </Layer>
+        <Layer>
+          <Line
+            points={[
+              offsetX + 50,
+              offsetY + (sofaTotalDepth / 1.4),
+              offsetX + (sofaTotalWidth / 2),
+              offsetY + (sofaTotalDepth / 1.4),
+            ]}
+            stroke="black"
+            strokeWidth={1}
+            closed={false}
+          />
+          <Line
+            points={[
+              offsetX - 50,
+              offsetY + (sofaTotalDepth / 1.4),
+              offsetX - (sofaTotalWidth / 2),
+              offsetY + (sofaTotalDepth / 1.4),
+            ]}
+            stroke="black"
+            strokeWidth={1}
+            closed={false}
+          />
+          <Text
+            x={offsetX - 50}
+            y={offsetY + (sofaTotalDepth / 1.4) - 9}
+            text={productWidth}
+            width={100}
+            align="center"
+            fontSize={18}
+          />
+          <Line
+            points={[
+              offsetX - (sofaTotalWidth / 1.7),
+              offsetY - (sofaTotalDepth / 2),
+              offsetX - (sofaTotalWidth / 1.7),
+              offsetY - 50,
+            ]}
+            stroke="black"
+            strokeWidth={1}
+            closed={false}
+          />
+          <Line
+            points={[
+              offsetX - (sofaTotalWidth / 1.7),
+              offsetY + 50,
+              offsetX - (sofaTotalWidth / 1.7),
+              offsetY + (sofaTotalDepth / 2),
+            ]}
+            stroke="black"
+            strokeWidth={1}
+            closed={false}
+          />
+          <Text
+            x={offsetX - (sofaTotalWidth / 1.7) - 9}
+            y={offsetY + 50}
+            text={productDepth}
+            width={100}
+            align="center"
+            rotation={270}
+            fontSize={18}
+          />
+        </Layer>
+      </Stage>
+    </div>
+  );
+})
+
+export default Tor;
